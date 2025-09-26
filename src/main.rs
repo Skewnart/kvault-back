87ae0f781d712b824e9748ce188f297763068e4a
Skewnart <corentinz@outlook.fr>
2025-09-26 17:20:32 +0200

    feat: implementing config to env / postgresql deps

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,46 @@
+use actix_web::{
+    App, Error, HttpResponse, HttpServer, Responder,
+    web::{self, ThinData},
+};
 use std::env;
 
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use confik::{Configuration as _, EnvSource};
+use deadpool_postgres::{Client, Pool};
+use tokio_postgres::NoTls;
+
+use crate::config::ExampleConfig;
+
+mod config;
+mod db;
+mod errors;
+mod models;
+
+use self::{errors::MyError, models::User};
+
+// TODO il faut mettre en place le logging avec les niveau de log
+// TODO Il faut absolument comprendre toute la mise en place de PG (tout est pris "à la base" des exemples du repo d'actix)
+// TODO donc ça comprend aussi la récupération des env et la mise en place de pg
+
+pub async fn get_users(ThinData(db_pool): web::ThinData<Pool>) -> Result<HttpResponse, Error> {
+    let client: Client = db_pool.get().await.map_err(MyError::PoolError)?;
+
+    let users = db::get_users(&client).await?;
+
+    Ok(HttpResponse::Ok().json(users))
+}
+
+pub async fn add_user(
+    user: web::Json<User>,
+    ThinData(db_pool): web::ThinData<Pool>,
+) -> Result<HttpResponse, Error> {
+    let user_info: User = user.into_inner();
+
+    let client: Client = db_pool.get().await.map_err(MyError::PoolError)?;
+
+    let new_user = db::add_user(&client, user_info).await?;
+
+    Ok(HttpResponse::Ok().json(new_user))
+}
 
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello, World !")
@@ -17,19 +57,43 @@ async fn main() -> std::io::Result<()> {
 
     let port = match args.next() {
         Some(port) => port.parse::<u16>().unwrap_or(5001),
-        _ => 5001
+        _ => 5001,
     };
 
     println!("Port utilisé : {port}");
 
-    HttpServer::new(
-        || App::new()
-            .service(
-                web::scope("/api")
-                    .route("", web::get().to(index))
-                    .route("{name}", web::get().to(hello))
-            ))
-        .bind(("0.0.0.0", port))?
-        .run()
-        .await
+    dotenvy::dotenv_override().ok();
+
+    let config = ExampleConfig::builder()
+        .override_with(EnvSource::new())
+        .try_build()
+        .unwrap();
+
+    let pool = config.pg.create_pool(None, NoTls).unwrap();
+
+    // # Test de connexion à la base au démarrage
+    println!("Test de connexion à la base PostgreSQL.");
+    match pool.get().await {
+        Ok(_) => println!("Connexion à la base PostgreSQL réussie."),
+        Err(e) => {
+            eprintln!("Erreur de connexion à la base PostgreSQL : {e}");
+            std::process::exit(1);
+        }
+    }
+
+    HttpServer::new(move || {
+        App::new().app_data(web::ThinData(pool.clone())).service(
+            web::scope("/api")
+                .service(
+                    web::resource("/users")
+                        .route(web::get().to(get_users))
+                        .route(web::post().to(add_user)),
+                )
+                .route("", web::get().to(index))
+                .route("{name}", web::get().to(hello)),
+        )
+    })
+    .bind(("0.0.0.0", port))?
+    .run()
+    .await
 }
