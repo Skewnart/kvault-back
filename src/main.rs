6d1ccea8079213f9969ef2c29ff46bf2d0e8299a
Skewnart <corentinz@outlook.fr>
2025-10-07 17:35:39 +0200

    feat: moving models into specific folder

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+mod database;
+mod models;
+
 use actix_web::{
-    App, Error, HttpResponse, HttpServer, Responder,
+    App, Error, HttpResponse, HttpServer,
     web::{self, ThinData},
 };
 use std::env;
@@ -8,22 +11,14 @@ use confik::{Configuration as _, EnvSource};
 use deadpool_postgres::{Client, Pool};
 use tokio_postgres::NoTls;
 
-mod config;
-mod db;
-mod errors;
-mod models;
-
-use crate::config::EnvConfig;
-
-use self::{errors::DbError, models::User};
+use self::models::{errors::DbError, config::EnvConfig, db::{user::User}};
 
 // TODO il faut mettre en place le logging avec les niveau de log
-// TODO Il faut absolument comprendre toute la mise en place de PG (tout est pris "à la base" des exemples du repo d'actix)
-// TODO donc ça comprend aussi la récupération des env et la mise en place de pg
+// TODO déplacer les actions dans des contrôleurs exprès (dossier controllers, models, services, resources)
 
 pub async fn get_users(ThinData(db_pool): web::ThinData<Pool>) -> Result<HttpResponse, Error> {
     let client: Client = db_pool.get().await.map_err(DbError::PoolError)?;
-    let users = db::get_users(&client).await?;
+    let users = database::get_users(&client).await?;
 
     Ok(HttpResponse::Ok().json(users))
 }
@@ -34,18 +29,18 @@ pub async fn add_user(
 ) -> Result<HttpResponse, Error> {
     let user_info: User = user.into_inner();
     let client: Client = db_pool.get().await.map_err(DbError::PoolError)?;
-    let new_user = db::add_user(&client, user_info).await?;
+    let new_user = database::add_user(&client, user_info).await?;
 
     Ok(HttpResponse::Ok().json(new_user))
 }
 
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello, World !")
-}
+// async fn index() -> impl Responder {
+//     HttpResponse::Ok().body("Hello, World !")
+// }
 
-async fn hello(name: web::Path<String>) -> impl Responder {
-    HttpResponse::Ok().body(format!("Hello {} !", &name))
-}
+// async fn hello(name: web::Path<String>) -> impl Responder {
+//     HttpResponse::Ok().body(format!("Hello {} !", &name))
+// }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -88,8 +83,8 @@ async fn main() -> std::io::Result<()> {
                             .route(web::get().to(get_users))
                             .route(web::post().to(add_user)),
                     )
-                    .route("", web::get().to(index))
-                    .route("{name}", web::get().to(hello)),
+                    // .route("", web::get().to(index))
+                    // .route("{name}", web::get().to(hello)),
         )
     })
     .bind(("127.0.0.1", port))?
