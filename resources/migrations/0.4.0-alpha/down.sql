bcfe32b0e063dfa48effc2fd4c490fd89ad0a252
Skewnart <corentinz@outlook.fr>
2026-01-12 14:56:06 +0100

    feat: entries table creation

--- resources/migrations/0.4.0-alpha/down.sql
+++ resources/migrations/0.4.0-alpha/down.sql
@@ -1,1 +1,2 @@
 DROP TABLE folders;
+DROP TABLE entries;
--- resources/migrations/0.4.0-alpha/up.sql
+++ resources/migrations/0.4.0-alpha/up.sql
@@ -6,3 +6,16 @@ CREATE TABLE folders (
     CONSTRAINT folders_unique UNIQUE ("name",user_id),
     CONSTRAINT folders_users_fk FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+CREATE TABLE entries (
+    id bigserial NOT NULL,
+    "name" text NOT NULL,
+    description text NULL,
+    "password" text NOT NULL,
+    is_favoris boolean DEFAULT false NOT NULL,
+    folder_id bigserial NOT NULL,
+    user_id bigserial NOT NULL,
+    CONSTRAINT entries_pk PRIMARY KEY (id),
+    CONSTRAINT entries_folders_fk FOREIGN KEY (folder_id) REFERENCES folders(id),
+    CONSTRAINT entries_users_fk FOREIGN KEY (user_id) REFERENCES users(id)
+);
