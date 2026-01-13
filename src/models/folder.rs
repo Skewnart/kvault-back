6caab597d6c177bd54753d762d595bcd9946b471
Skewnart <corentinz@outlook.fr>
2026-01-13 11:39:09 +0100

    feat: adding entry model

--- src/models/folder.rs
+++ src/models/folder.rs
@@ -2,14 +2,14 @@ use serde::{Deserialize, Serialize};
 use tokio_pg_mapper_derive::PostgresMapper;
 
 #[derive(Deserialize, PostgresMapper, Serialize)]
-#[pg_mapper(table = "folder")]
+#[pg_mapper(table = "folders")]
 pub struct FolderOutputDTO {
     pub id: i64,
     pub name: String,
 }
 
 #[derive(Deserialize, PostgresMapper, Serialize)]
-#[pg_mapper(table = "folder")]
+#[pg_mapper(table = "folders")]
 pub struct FolderDetailOutputDTO {
     pub name: String,
 }
