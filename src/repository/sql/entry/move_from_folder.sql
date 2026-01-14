6e6a4b2042e18680b1a87b992b275c6c7d8fed53
Skewnart <corentinz@outlook.fr>
2026-01-14 18:39:02 +0100

    feat: updating sql files to exclude folder injections

--- src/repository/sql/entry/move_from_folder.sql
+++ src/repository/sql/entry/move_from_folder.sql
@@ -1,5 +1,11 @@
 UPDATE entries
-SET folder_id = $1
+SET folder_id =
+(
+    SELECT id
+    FROM folders
+    WHERE id = $1
+    AND user_id = $3
+)
 WHERE id = $2
   AND user_id = $3
 RETURNING id;
