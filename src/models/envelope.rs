c11599b46a679613f5747042dae5a76508f4c55f
Skewnart <corentinz@outlook.fr>
2026-03-29 23:45:14 +0200

    feat: removing useless wraping on envelope

--- src/models/envelope.rs
+++ src/models/envelope.rs
@@ -1,10 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
-pub struct EnvelopeDTO {
-    pub envelope: serde_json::Value,
-}
-
 #[derive(Deserialize, Serialize)]
 pub struct EncodedDataDTO {
     pub enc_data: serde_json::Value,
--- src/repository/envelope_repository.rs
+++ src/repository/envelope_repository.rs
@@ -1,8 +1,7 @@
 use crate::errors::db_error::DbError;
-use crate::models::envelope::EnvelopeDTO;
 use deadpool_postgres::Client;
 
-pub async fn get_by_user_id(client: &Client, user_id: i64) -> Result<EnvelopeDTO, DbError> {
+pub async fn get_by_user_id(client: &Client, user_id: i64) -> Result<serde_json::Value, DbError> {
     let _stmt = include_str!("sql/envelope/get.sql");
     let _stmt = client.prepare(_stmt).await?;
 
@@ -15,7 +14,5 @@ pub async fn get_by_user_id(client: &Client, user_id: i64) -> Result<EnvelopeDTO
         .pop()
         .ok_or(DbError::NotFound)?;
 
-    let envelope_dto = EnvelopeDTO { envelope };
-
-    Ok(envelope_dto)
+    Ok(envelope)
 }
